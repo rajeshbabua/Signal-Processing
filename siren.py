ff9bf03e4450dbfe18f1fe2e44be386cf85196fc
RajeshBabu_A <32333617+rajeshbabua@users.noreply.github.com>
2018-09-08 21:11:04 +0530

    Update siren.py

--- siren.py
+++ siren.py
@@ -1,6 +1,12 @@
 from scipy.io.wavfile import write
 from numpy import arange, pi, sin, int16
 
+
+
+#################the process of defining a signal
+####carrier signal is a modulated signal which contains message signal..
+####modulaition is a process of varying carrier a signal with modulatiing signal that contains information to be transferred
+
 def f(t, f_c, f_m, beta):
     # t    = time
     # f_c  = carrier frequency
@@ -16,6 +22,8 @@ def to_integer(signal):
 N = 48000 # samples per second
 x = arange(3*N) # three seconds of audio
 
+################time t=x/N
+############varying the frequency of the signal one can get different signals
 data = f(x/N, 1500, 2, 100)
 write("slow.wav", N, to_integer(data))
 
